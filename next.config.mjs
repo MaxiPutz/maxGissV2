17333f518b3e15d152cfc0ab77c364f3a9c0706a
MaxiPutz <48091139+MaxiPutz@users.noreply.github.com>
2025-12-12 18:18:34 +0100

    Enhance next.config.mjs with dynamic paths
    Add basePath and assetPrefix configuration options.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH;
+const assetPrefix = process.env.NEXT_PUBLIC_ASSET_PREFIX;
+
 const nextConfig = {
-    reactStrictMode: true,
-    transpilePackages: ['@reduxjs/toolkit'],
+  reactStrictMode: true,
+  transpilePackages: ["@reduxjs/toolkit"],
+
+  ...(basePath ? { basePath } : {}),
+  ...(assetPrefix ? { assetPrefix } : {}),
 };
 
 export default nextConfig;
-  
